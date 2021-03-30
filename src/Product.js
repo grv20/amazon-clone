4effd3bbc790a667645dabac39e8db96f86bfd8c
grv20 <windroid20@gmail.com>
2021-03-30 12:04:57 +0530

    subtotal part

--- src/Product.js
+++ src/Product.js
@@ -1,12 +1,25 @@
 import React from "react";
 import "./Product.css";
-function Product({ title, image, price, rating }) {
-  if (rating) {
-    let i;
-    for (i in rating) {
-      console.log(i);
-    }
-  }
+import { useStateValue } from "./StateProvider";
+
+function Product({ id, title, image, price, rating }) {
+  const [dispatch] = useStateValue();
+
+  //console.log("Basket >>> ", basket);
+
+  const addToBasket = () => {
+    //dispatch the item into the data layer
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
   return (
     <div className="product">
       <div className="product__info">
@@ -24,7 +37,7 @@ function Product({ title, image, price, rating }) {
         </div>
       </div>
       <img src={image} alt="" />
-      <button>Add to Basket</button>
+      <button onClick={addToBasket}>Add to Basket</button>
     </div>
   );
 }
--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,15 +1,20 @@
 import React from "react";
 import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
+import { useStateValue } from "./StateProvider";
+import { getBaketTotal } from "./reducer";
 
 function Subtotal() {
+  const [{ basket }] = useStateValue();
+
+  console.log(basket);
   return (
     <div className="subtotal">
       <CurrencyFormat
         renderText={(value) => (
           <>
             <p>
-              Subtotal (0 items): <strong>0</strong>
+              Subtotal ({basket.length} items): <strong>{value}</strong>
             </p>
             <small className="subtotal__gift">
               <input type="checkbox" /> This order contains a gift
@@ -17,7 +22,7 @@ function Subtotal() {
           </>
         )}
         decimalScale={2}
-        value={0}
+        value={getBaketTotal(basket)}
         displayType={"text"}
         thousandSeperator={true}
         prefix={"₹"}
