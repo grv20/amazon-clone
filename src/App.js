04fac2affa929f0b6714a679ab12ffd1aad78973
grv20 <windroid20@gmail.com>
2021-04-13 15:58:59 +0530

    login front-end

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
 import Checkout from "./Checkout";
+import Login from "./Login";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
@@ -10,6 +11,9 @@ function App() {
     <Router>
       <div className="App">
         <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
           <Route path="/checkout">
             <Header />
             <Checkout />
